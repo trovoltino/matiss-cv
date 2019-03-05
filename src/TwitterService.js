ba25d9e867e4cbabac7d0342d7a920f2d00b2537
matiiss-z <matiissdz@gmail.com>
2019-03-05 19:47:27 +0200

    This might brake things

---
+++ src/TwitterService.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+const url = 'http://localhost:5000/api/posts/';
+
+class TwitterService {
+  static getTweets(){
+    return new Promise(async (resolve, reject) =>{
+      try {
+        const res = await axios.get(url);
+        const data = res.data;
+        resolve(
+          data.map(post =>({
+            ...post,
+            createdAt: new Date(post.createdAt)
+          }))
+        );
+      } catch(err) {
+        reject(err);
+      }
+    });
+  }
+
+  static insertTweet(text){
+    return axios.post(url, {
+      text
+    });
+  }
+
+  static deleteTweet(id) {
+    return axios.delete(`${url}${id}`)
+  }
+  
+}
+
+export default TwitterService
--- src/views/CryptoApi.vue
+++ src/views/CryptoApi.vue
@@ -2,27 +2,63 @@
   <div class="crypto">
     <div class="crypto-wrap">
       <h1>Crytpo information daily</h1>
-      <h2>{{cryptoData}}</h2>
+      <h2>cryptoData</h2>
+    </div>
+    <div class="create-post">
+      <label for="create-post">Say something...</label>
+      <input type="text" id="create-post" v-model="text" placeholder="Create a post">
+      <button @click="createTweet">Post!</button>
+    </div>
+    <div class="tweet-container">
+      <h2>Tweets you love</h2>
+      <p class="error" v-if="error">{{ error }}</p>
+      <div class="tweets"
+        v-for="(tweet, index) in tweets"
+        v-bind:item="tweet"
+        v-bind:index="index"
+        v-bind:key="tweet._id"
+        @dblclick="deleteTweet(tweet._id)"
+      >
+        {{ `${tweet.createdAt.getDate()}/${tweet.createdAt.getMonth()}/${tweet.createdAt.getFullYear()}` }}
+        <p class="text">{{ tweet.text }}</p>
+        <p>{{ index }}</p>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import axios from 'axios'
+
+import TwitterService from '../TwitterService.js';
 
 export default {
-  name: 'Crypto',
+  name: 'TweetComponent',
   components: {
   },
   data() {
     return {
-      cryptoData: null
+      tweets: [],
+      error: '',
+      text: ''
     }
   },
-  mounted () {
-    axios.get('https://api.coindesk.com/v1/bpi/currentprice.json')
-    .then(response => (this.cryptoData = response))
+  async created(err) {
+    try {
+      this.tweets = await TwitterService.getTweets();
+    } catch {
+      this.error = err.message;
+    }
+  },
+  methods: {
+    async createTweet() {
+      await TwitterService.insertTweet(this.text);
+      this.tweets = await TwitterService.getTweets();
+    },
+    async deleteTweet(id) {
+      await TwitterService.deleteTweet(id);
+      this.tweets = await TwitterService.getTweets();
+    }
   }
 }
 </script>
@@ -30,12 +66,24 @@ export default {
 <style>
 
 .crypto-wrap{
-  position: absolute;
+  display: none;
+  position: relative;
   top: 10%;
   left:5%;
   border: 3px gray groove;
   width: 600px;
   height: 400px;
   overflow: auto;
+}
+.tweet-container {
+  display: flex;
+  flex-direction: column;
+  height: 600px;
+  overflow: auto;
+  align-items: center;
+}
+.tweets {
+  border: 2px rgb(90, 90, 255) groove;
+  width: 30%;
 }
 </style>
