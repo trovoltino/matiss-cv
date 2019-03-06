ea5d44073abd43de9e686173258df70c757e4f0b
matiiss-z <matiissdz@gmail.com>
2019-03-06 19:21:29 +0200

    server implementation

--- server/index.js
+++ server/index.js
@@ -12,6 +12,16 @@ const posts = require('./routes/api/posts');
 
 app.use('/api/posts', posts);
 
+// Handle Production
+
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static(__dirname + '/dist/'));
+}
+
+// Handle SPA
+
+app.get(/.*/, (req, res) => res.sendFile(__dirname + '/dist/index.html'));
+
 const port = process.env.PORT || 5000;
 
 app.listen(port, ()=> console.log(`Server started on port ${port}`));
